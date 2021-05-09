cfb5b9c9572a61d0d30120ae08b8e3d750050f3e
niekurzakdaniel <niekurzakdaniel@gmail.com>
2021-05-09 23:07:19 +0200

    add user view, add restaurant and meals preview for customer

--- SmartDelivery.WEB/Areas/Customer/Controllers/HomeController.cs
+++ SmartDelivery.WEB/Areas/Customer/Controllers/HomeController.cs
@@ -1,25 +1,70 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using SmartDelivery.Infrastructure.Common.Pagination;
+using SmartDelivery.Infrastructure.Services.Interfaces;
 using SmartDelivery.WEB.Models;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace SmartDelivery.WEB.Controllers
 {
     [Area("Customer")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IDishService _dishService;
+        private readonly IRestaurantService _restaurantService;
 
-        public HomeController(ILogger<HomeController> logger)
+        private const int PageSize = 9;
+        public HomeController(IDishService productService, IRestaurantService shopService)
         {
-            _logger = logger;
+            _dishService = productService;
+            _restaurantService = shopService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int productPage = 1)
         {
-            return View();
+            // var (products, productsCount) = await _dishService.GetFiltered(searchByProduct, searchByCategory, searchByShop, PageSize, productPage);
+
+            var restaurants = await _restaurantService.GetAllDetails();
+            var homeVM = new HomeViewModel()
+            {
+                Restaurants= restaurants,
+            };
+
+            const string Url = "/User/Home/Index?productPage=:";
+
+            homeVM.PagingInfo = new PagingInfo
+            {
+                CurrentPage = productPage,
+                ItemsPerPage = PageSize,
+                TotalItem = 10,
+                UrlParam = Url
+            };
+
+            return View(homeVM);
         }
 
+        public async Task<IActionResult> Meals(int? id)
+        {
+            //var (products, productsCount) = await _dishService.GetFiltered(productName, categoryName, PageSize, productPage);
+
+            var dishList = new DishViewModel()
+            {
+                Products = await _restaurantService.GetMealsByRestaurant(id)
+            };
+
+            const string Url = "/Home/Meals?productPage=:";
+
+            dishList.PagingInfo = new PagingInfo
+            {
+                CurrentPage = 1,
+                ItemsPerPage = PageSize,
+                TotalItem = 10,
+                UrlParam = Url
+            };
+
+             return View(dishList);
+        }
+ 
         public IActionResult Privacy()
         {
             return View();
---
+++ SmartDelivery.WEB/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SmartDelivery.Data.Entities;
+using SmartDelivery.Infrastructure.Common.Pagination;
+using System.Collections.Generic;
+
+
+namespace SmartDelivery.WEB.Models
+{
+    public class HomeViewModel
+    {
+        public IList<Restaurant> Restaurants{ get; set; }
+
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
