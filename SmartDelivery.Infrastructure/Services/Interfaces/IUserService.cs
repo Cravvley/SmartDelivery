67b7096b354de5efbbee17e313df0adc6f42dbda
niekurzakdaniel <niekurzakdaniel@gmail.com>
2021-05-09 17:06:11 +0200

    add user service

---
+++ SmartDelivery.Infrastructure/Services/Interfaces/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using SmartDelivery.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SmartDelivery.Infrastructure.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> Get(string id);
+
+        Task<bool> Lock(string userId, bool lockUser);
+        
+        Task Delete(string id);
+
+        Task<IList<User>> GetAll(Expression<Func<User, bool>> filter = null);
+
+        Task<IList<User>> GetPaginated(Expression<Func<User, bool>> filter = null, int pageSize = 0, int productPage = 0);
+
+        Task<(IList<User> users, int usersCount)> GetFiltered(string userId, string userEmail = null, int? pageSize = null, int? productPage = null);
+    }
+}
